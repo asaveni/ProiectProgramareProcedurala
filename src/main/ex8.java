1d0d79f45cb6b34d1ddc606b1a627dc2dd8c4a41
Asa <Asa@ASA>
2020-02-02 00:02:58 +0200

    zilele saptamanii

---
+++ src/main/ex8.java
@@ -0,0 +1,33 @@
+package main;
+
+public class ex8 {
+
+	public static void main(String[] args) {
+		int ziua = 4;
+		switch (ziua) {
+		case 1:
+			System.out.println("Luni");
+			break;
+		case 2:
+			System.out.println("Marti");
+			break;
+		case 3:
+			System.out.println("Miercuri");
+			break;
+		case 4:
+			System.out.println("Joi");
+			break;
+		case 5:
+			System.out.println("Vineri");
+			break;
+		case 6:
+			System.out.println("Sambata");
+			break;
+		case 7:
+			System.out.println("Duminica");
+			break;
+		}// tiparirea zilei din saptamana
+
+	}
+
+}
