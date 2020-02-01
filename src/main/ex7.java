d0f88ddea1f2a1d9b6193dcb639262460aaabde8
Asa <Asa@ASA>
2020-02-02 00:00:49 +0200

    nr par sau impar

---
+++ src/main/ex7.java
@@ -0,0 +1,14 @@
+package main;
+
+public class ex7 {
+
+	public static void main(String[] args) {
+		int nr = 23; // verificarea daca nr este par sau impar
+		if (nr % 2 == 0)
+			System.out.println("numarul este par.");
+			else 
+				System.out.println("numarul este impar.");
+
+	}
+
+}
