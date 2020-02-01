e242e64ca8c622eed5ea799dc28aa1d4e6f057fb
Asa <Asa@ASA>
2020-02-01 23:17:03 +0200

    nu afiseaza nimic 4=4

---
+++ src/main/ex3.java
@@ -0,0 +1,17 @@
+package main;
+
+public class ex3 {
+
+	public static void main(String[] args) {
+		int a = 3;
+		if(++a < 4)
+			System.out.println(a);
+		else
+			if(a++<4)
+				
+			
+				System.out.println(a);
+
+	}
+
+}
