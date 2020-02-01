85495f7ea7c2977f29d5e81060842d3d9b94e5fa
Asa <Asa@ASA>
2020-02-01 23:26:14 +0200

    incrementarea cu valoarea operandului drept suma este 1013

---
+++ src/main/ex4.java
@@ -0,0 +1,15 @@
+package main;
+
+public class ex4 {
+
+	public static void main(String[] args) {
+		int suma = 0;
+		for(int i = 1; i<10; i++) {
+			suma+=suma+i;
+		}
+		System.out.println("suma este: " + suma);
+		// TODO Auto-generated method stub
+
+	}
+
+}
