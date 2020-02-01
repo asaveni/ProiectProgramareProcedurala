fefcb2d28b099c4c0d4f7740ccf3769f27fc20cd
Asa <Asa@ASA>
2020-02-01 23:42:15 +0200

    calculul factorial pentru n<13 n!=479001600

---
+++ src/main/ex5.java
@@ -0,0 +1,15 @@
+package main;
+
+public class ex5 {
+
+	public static void main(String[] args) {
+		int i,fact =1;
+		int n=13;
+		for(i=1; i<n; i++) {
+			fact=fact * i;
+			
+		}
+		System.out.println("n!=" + fact);
+	}
+}
+
