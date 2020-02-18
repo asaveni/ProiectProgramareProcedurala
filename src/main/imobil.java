d0dd60d81f68b676afa0462b30440b2707af8928
Asa <Asa@ASA>
2020-02-18 22:26:19 +0200

    imobil

---
+++ src/main/imobil.java
@@ -0,0 +1,42 @@
+package main;
+public class Imobil {
+	private String tipCasa;
+	private int nrEtaje;
+	private double pret;
+
+
+	public Imobil() {
+		tipCasa="";
+		nrEtaje=0;
+		pret=0;
+	}
+	
+
+	public Imobil(String tipCasa, int nrEtaje, double pret) {
+		this.tipCasa=tipCasa;
+		this.nrEtaje=nrEtaje;
+		this.pret=pret;
+	}
+	
+public String getTipCasa() {
+	return tipCasa;
+}
+public void setTipCasa(String tipCasa) {
+	this.tipCasa=tipCasa;
+}
+
+public int getNrEtaje() {
+	return nrEtaje;
+}
+public void setNrEtaje(int nrEtaje) {
+	this.nrEtaje=nrEtaje;
+}
+
+public double getPret() {
+	return pret;
+}
+public void setPret(double pret) {
+	this.pret=pret;
+}
+
+}
